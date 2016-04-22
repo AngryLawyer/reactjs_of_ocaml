3671a0e4ec59b353d100188ead1881973991cc13
Tony Aldridge <tony@dabapps.com>
2016-04-22 13:58:08 +0100

    Example of using functor-style create_class

--- test/test.ml
+++ test/test.ml
@@ -2,8 +2,19 @@ let create_element () =
     let element = (ReactJS.create_element (ReactJS.Tag_name "div") []) in
     Alcotest.(check bool) "is a valid element" (ReactJS.is_valid_element element) true
 
+
+module My_class = ReactJS.Make_ReactClass(struct type t = <  > end)
+let create_class () =
+    let react_class = My_class.create_class(object%js (self)
+        method render =
+            (ReactJS.create_element (ReactJS.Tag_name "div") [])
+    end) in
+    ()
+
+
 let reactjs_test_set = [
     "create_element", `Quick, create_element;
+    "create_class", `Quick, create_class;
 ]
 
 let () =
