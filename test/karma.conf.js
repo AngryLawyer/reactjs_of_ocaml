e39d56e1ef256c6cabcc7ccd0acd167aff6a801b
Tony Aldridge <tony@dabapps.com>
2016-05-04 13:00:29 +0100

    Corrected karma config

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      '../_build/test/test.js'
+      './test.js'
     ],
 
 
@@ -27,7 +27,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '../_build/test/test.js': ['browserify']
+      './test.js': ['browserify']
     },
 
 
